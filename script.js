b8db71d5eda907d8d3d19f3194c66b89af39ad91
Draconic-Hacker <kleberandrade512@gmail.com>
2025-05-09 08:19:26 -0300

    acionamento da animação ao carregar a página (alteração feita no js)

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const menu=document.querySelector(".menu");
-const toggle=document.querySelector(".toggle");
-toggle.addEventListener("click",()=>{
+const menu = document.querySelector(".menu");
+const toggle = document.querySelector(".toggle");
+
+// Adiciona a classe 'active' ao carregar a página
+document.addEventListener("DOMContentLoaded", () => {
+  menu.classList.add("active");
+});
+
+toggle.addEventListener("click", () => {
   menu.classList.toggle("active");
-})
+});
